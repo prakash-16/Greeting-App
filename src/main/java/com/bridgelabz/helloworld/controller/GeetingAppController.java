ffec7862bc4d82bc61973c1ec2c87f505cc9d2de
Prakash Goud <prakash.goud@morningstar.com>
2021-09-28 20:49:20 +0530

    [Prakash] - Added the ability to give greeting message with and without name

--- src/main/java/com/bridgelabz/helloworld/controller/GeetingAppController.java
+++ src/main/java/com/bridgelabz/helloworld/controller/GeetingAppController.java
@@ -2,6 +2,8 @@ package com.bridgelabz.helloworld.controller;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.json.simple.JSONObject;
 
@@ -14,8 +16,23 @@ public class GeetingAppController {
 		job.put(firstName, lastName);
 		return job;
 	}
+
 	@RequestMapping("/welcome")
 	public String welcomeMssg() {
 		return "Hello World";
+	}
+
+	@RequestMapping(value = {"/greet/user"}, method = RequestMethod.GET)
+	public String greetUser(@RequestParam(value = "firstName", required = false) String firstName, @RequestParam(value = "lastName", required = false) String lastName) {
+		if(firstName == null && lastName == null) {
+			return "Hello World";
+		}
+		else if(firstName == null) {
+			return "Hello Mr." + lastName;
+		}
+		else if(lastName == null) {
+			return "Hello Mr." + firstName;
+		}
+		return "Hello " + firstName + " " + lastName +" !";
 	}
 }

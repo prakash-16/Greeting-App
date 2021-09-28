a6bdfe45eb123ed2597c3b01cb02ca33ca1a56de
Prakash Goud <prakash.goud@morningstar.com>
2021-09-28 16:09:40 +0530

    [Prakash] - Added the feature to return Json objects

---
+++ src/main/java/com/bridgelabz/helloworld/controller/GeetingAppController.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.helloworld.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.json.simple.JSONObject;
+
+@RestController
+public class GeetingAppController {
+	@RequestMapping("/json/param/{firstName}/{lastName}")
+	public JSONObject jsonObject(@PathVariable String firstName, @PathVariable String lastName) {
+		JSONObject job = new JSONObject();
+		job.put(firstName, lastName);
+		return job;
+	}
+}
